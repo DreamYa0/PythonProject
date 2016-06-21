d7f8bc134d67bfe21bfcd829870ae5df825e5457
Administrator <805229888@qq.com>
2016-06-21 15:50:04 +0800

    入门

--- TestPython/practice/20160621test001.py
+++ TestPython/practice/20160621test001.py
@@ -8,6 +8,7 @@ import sys;x = 'roob';sys.stdout.write(x + '\n')
 
 
 #列表：加号（+）是列表连接运算符，星号（*）是重复操作
+
 list=[1,2,3,4,]
 list1=['a','b']
 print(list)
@@ -15,4 +16,27 @@ print(list[2])
 print(list1 * 2)
 print(list + list1)
 
+#列表中的元素是可以改变的
 
+a=[1,2,3,4,5]
+a[0]=9
+print(a)
+
+#元组
+
+i=('ab','cd','ef')
+print (i[0:2])                        #i(0)=(5)；print (i)  修改元组的元素是非法的操作
+
+#字符串
+
+te='hello,boy'
+print(te[0:2])
+
+#集合（set）是一个无序不重复元素的序列
+
+student={'mm','nn','bb','vv','mm'}
+print(student)                    #输出集合，重复的元素被自动去掉
+if ('aa' in student) :
+      print('aa在集合中')
+else:
+      print('aa不在集合中')
---
+++ TestPython/practice/20160621test002.py
@@ -0,0 +1,15 @@
+#python输入与输出
+#两种输出值的方式: 表达式语句和 print() 函数。第三种方式是使用文件对象的 write() 方法
+#标准输出文件可以用 sys.stdout 引用。 str.format() 函数来格式化输出值。将输出的值转成字符串，可以使用 repr()(解释器易读) 或 str() (用户易读)函数
+
+a=25;b=10
+s='a的值为: ' + repr(a) + ' , b的值为:'+ repr(b) +''
+print(s)
+
+aa='hi\n'                   #repr()函数可以转义字符串中的特殊字符
+bb=repr(aa)
+print (bb)
+
+aa='h\ni'
+print (aa)                   #\n 换行
+
