82fce6e3a55e8a810ed63e29cab0dc3dd8c5e2bf
Administrator <805229888@qq.com>
2016-06-21 12:00:01 +0800

    入门

--- TestPython/practice/20160621test001.py
+++ TestPython/practice/20160621test001.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 import sys;x = 'roob';sys.stdout.write(x + '\n')
 
-#可能是之前包没关联上
+
 #标准数据类型
 #Number（数字）String（字符串）List（列表）Tuple（元组）Sets（集合）Dictionary（字典）
+
+
+
+#列表：加号（+）是列表连接运算符，星号（*）是重复操作
+list=[1,2,3,4,]
+list1=['a','b']
+print(list)
+print(list[2])
+print(list1 * 2)
+print(list + list1)
+
+
