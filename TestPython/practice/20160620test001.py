367a72808a5539367933f14975470115033c6226
yaoijun <yaojun@ebigdata.org>
2016-06-26 22:56:00 +0800

    Add Some File

--- TestPython/practice/20160620test001.py
+++ TestPython/practice/20160620test001.py
@@ -1,5 +1,5 @@
-print('hi,girl!')  #入门
+print('hi,girl!')  # 入门
 
-#total = item_one + \    #多行语句.我们可以使用反斜杠(\)来实现多行语句
- #       item_two + \
-  #      item_three
+# total = item_one + \    #多行语句.我们可以使用反斜杠(\)来实现多行语句
+#       item_two + \
+#      item_three
--- TestPython/practice/20160621test001.py
+++ TestPython/practice/20160621test001.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
-import sys;x = 'roob';sys.stdout.write(x + '\n')
+import sys
+
+x = 'roob'
+sys.stdout.write(x + '\n')
+
+# 标准数据类型
+# Number（数字）String（字符串）List（列表）Tuple（元组）Sets（集合）Dictionary（字典）
 
 
-#标准数据类型
-#Number（数字）String（字符串）List（列表）Tuple（元组）Sets（集合）Dictionary（字典）
+# 列表：加号（+）是列表连接运算符，星号（*）是重复操作
 
-
-#列表：加号（+）是列表连接运算符，星号（*）是重复操作
-
-list=[1,2,3,4,]
-list1=['a','b']
+list = [1, 2, 3, 4, ]
+list1 = ['a', 'b']
 print(list)
 print(list[2])
 print(list1 * 2)
 print(list + list1)
 
-#列表中的元素是可以改变的
+# 列表中的元素是可以改变的
 
-a=[1,2,3,4,5]
-a[0]=9
+a = [1, 2, 3, 4, 5]
+a[0] = 9
 print(a)
 
-#元组
+# 元组
 
-i=('ab','cd','ef')
-print (i[0:2])
+i = ('ab', 'cd', 'ef')
+print(i[0:2])
 
-#i(0)=(5)；print (i)  修改元组的元素是非法的操作
+# i(0)=(5)；print (i)  修改元组的元素是非法的操作
 
-#字符串
+# 字符串
 
-te='hello,boy'
+te = 'hello,boy'
 print(te[0:2])
 
-#集合（set）是一个无序不重复元素的序列
+# 集合（set）是一个无序不重复元素的序列
 
-student={'mm','nn','bb','vv','mm'}
-print(student)                    #输出集合，重复的元素被自动去掉
-if ('aa' in student) :
-      print('aa在集合中')
+student = {'mm', 'nn', 'bb', 'vv', 'mm'}
+print(student)  # 输出集合，重复的元素被自动去掉
+if 'aa' in student:
+    print('aa在集合中')
 else:
-      print('aa不在集合中')
-
+    print('aa不在集合中')
