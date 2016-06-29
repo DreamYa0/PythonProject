9fd880e0c9367a481d520d9fb3a264d459775a78
Administrator <805229888@qq.com>
2016-06-29 14:59:55 +0800

    入门

--- TestPython/practice/init2.py
+++ TestPython/practice/init2.py
@@ -1,4 +1,10 @@
 # coding:utf-8
-str='bad bad bad'
-str=str.encode('GBK','strict')
+str = 'bad bad bad'
+str = str.encode('GBK', 'strict')
 print(str)
+
+word = ['a', 'b', 'c', 'd', 'e']
+A = word[0:2]
+print(A)
+B = word[:3]
+print('word[:3]:', B)
