1eae18fbe0b6c5f4ac32e637c1b4af2acbe74518
Administrator <805229888@qq.com>
2016-07-04 18:32:48 +0800

    入门

--- TestPython/practice/if_ input.py
+++ TestPython/practice/if_ input.py
@@ -2,13 +2,13 @@
 wages = int(input('请输入你的工资'))
 print('')
 if wages < 0:
-   print('你还真是个败家子')
+    print('你还真是个败家子')
 elif wages == 0:
-   print ('你是月光族')
+    print('你是月光族')
 elif wages <= 4000:
-   print('你的工资低于平均水平')
+    print('你的工资低于平均水平')
 elif wages > 4000:
-   print('你的工资等于平均水平')
+    print('你的工资等于平均水平')
 elif wages >= 5000:
     print('你的工资高于平均水平')
 # 退出
