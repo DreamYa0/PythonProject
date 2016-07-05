795dd91171b52462a0dccd566cdd9964819f2b93
Administrator <805229888@qq.com>
2016-07-05 14:15:24 +0800

    新建文件while_sum.py到目录1下

--- TestPython/practice/1/if_ture_false_.py
+++ TestPython/practice/1/if_ture_false_.py
@@ -10,3 +10,16 @@ while game != number:
         print('您输入的数字大了')
     elif game < number:
         print('您输入的数字小了')
+
+# if 嵌套
+number1 = int(input('请输入一个数字'))
+if number1 >50:
+    if number1 %2 ==0:
+        print('您输入的是一个大于50且能整除2的数')
+    else:
+        print('您输入的是一个大于50的数')
+else:
+    if number1 %3 ==0:
+        print('您输入的是一个小于50且整除3的数')
+    else:
+        print('您输入的是一个小于50的数')
---
+++ TestPython/practice/1/while_sum.py
@@ -0,0 +1,16 @@
+# 1到100求和
+n = 1
+sum = 0
+while n < 101:
+    sum = sum + n
+    n +=1
+print(sum)
+
+# 1~100偶数之和
+n1 = 1
+sum1 = 0
+while n1 <101:
+    if n1 %2==0:
+        sum1 +=n1
+    n1 += 1
+print(sum1)
