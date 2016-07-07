6674accab7d61a773f86d0987d33eb7febda4300
Administrator <805229888@qq.com>
2016-07-07 18:07:05 +0800

    新增文件input_and_raw_input_.py到目录2下

---
+++ TestPython/practice/02/input_and_raw_input.py
@@ -0,0 +1,10 @@
+import cmath
+# 3.0版本已经没有raw_input
+num = int(input("请输入一个数字: "))
+num_sqrt = cmath.sqrt(num)
+print('{0} 的平方根为 {1:0.3f}+{2:0.3f}j'.format(num, num_sqrt.real, num_sqrt.imag))
+
+a = 3
+b = 5
+c = a ** 3 + b
+print(c)
