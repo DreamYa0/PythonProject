3b30e3eab2492c0d9a4684a57ad6f1eb7eb1a713
DreamYao <805229888@qq.com>
2016-10-30 21:53:27 +0800

    修改ReadExcel

--- TestPython/practice/ReadExcel.py
+++ TestPython/practice/ReadExcel.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+"""
+Author:姚军
+"""
 import json
 import xlrd
 
@@ -12,25 +16,27 @@ def open_excel(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTemplate.xl
 
 
 # 根据索引获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_index：表的索引
-def excel_table_byindex(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTemplate.xlsx', colnameindex=0, by_index=0):
+def excel_table_byindex(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTemplate.xlsx', colnameindex=0,
+                        by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows  # 行数
     colnames = table.row_values(colnameindex)  # 某一行数据
     list = []
     for rownum in range(1, nrows):
-
         row = table.row_values(rownum)
         if row:
             app = {}
             for i in range(len(colnames)):
                 app[colnames[i]] = row[i]
-            list.append(json.dumps(app, encoding='UTF-8', ensure_ascii=False))
+            # list.append(json.dumps(app, encoding='UTF-8', ensure_ascii=False))
+            list.append(app)
     return list
 
 
 # 根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
-def excel_table_byname(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTemplate.xlsx', colnameindex=0, by_name=u'Sheet1'):
+def excel_table_byname(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTemplate.xlsx', colnameindex=0,
+                       by_name=u'Sheet1'):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
     nrows = table.nrows  # 行数
@@ -49,7 +55,7 @@ def excel_table_byname(file='E:\\pjz\\DubboInterfaceTest\\Testcases\\TestcaseTem
 def main():
     tables = excel_table_byindex()
     for row in tables:
-        print row
+        print json.dumps(row[u'\u53c2\u6570'])
 
 
 if __name__ == "__main__":
